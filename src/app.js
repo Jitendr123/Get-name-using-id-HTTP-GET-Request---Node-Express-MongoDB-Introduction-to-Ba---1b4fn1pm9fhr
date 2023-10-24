9f856db55347fa5fc62e40463a475efae360bf52
Jitendra Arya <114763869+Jitendr123@users.noreply.github.com>
2023-10-24 16:32:46 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ const app = express();
 const productNames = JSON.parse(
     fs.readFileSync(`${__dirname}/data/names.json`)
 );
+app.get('/api/v1/names:id',async(req,res)=>{
+    const productId=req.params.id.split(":");
+    console.log(productId[1])
+    const product=await productNames.find(product=>product.id==productId[1])
+    console.log(product,'hello')
+    if(product){
+        res.statusCode=200
+        res.json({    
+            status: "success", 
+            message: "Product name fetched successfully",
+            data: { 
+                id: product.id,
+                name:product.name
+             } 
+        })
+    }else{
+        res.statusCode=404
+        res.json({ 
+            status: "failed", 
+            message: "Not found!" 
+        })
+    }
+    res.statusCode=200;
+    res.end('hello');
+})
 
 //Middlewares
 app.use(express.json())
